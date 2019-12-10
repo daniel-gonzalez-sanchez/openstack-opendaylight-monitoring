fc1a04fdac8bc99a1e40e5784f5af97ef1219ef5
Daniel González Sánchez <55086789+daniel-gonzalez-sanchez@users.noreply.github.com>
2019-12-10 14:37:31 +0100

    Delete create.sh

--- openstack_opendaylight_lab/filesystems/create.sh
+++
@@ -1,7 +0,0 @@
-sudo ./create_rootfs_lxc_ubuntu64-18.04-ostack-controller
-sleep 5
-sudo ./create_rootfs_lxc_ubuntu64-18.04-ostack-network
-sleep 5
-sudo ./create_rootfs_lxc_ubuntu64-18.04-ostack-compute
-sleep 5
-sudo ./create_rootfs_lxc_ubuntu64-18.04-ostack-opendaylight
